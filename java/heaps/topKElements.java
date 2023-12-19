75e7a54de18e88901cbe0310c10f3f1b844f68c8
AxelLWitt <axellwitt16@gmail.com>
2023-12-19 16:37:08 -0500

    optimization

--- java/heaps/topKElements.java
+++ java/heaps/topKElements.java
@@ -29,3 +29,50 @@ class Solution {
         return kels;
     }
 }
+
+
+//optimized
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length == 1) {
+            return nums;
+        }
+
+        // Frequency map
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        int maxFreq = 0;
+        for(int num : nums) {
+            int freq = frequencyMap.getOrDefault(num, 0) + 1;
+            frequencyMap.put(num, freq);
+            maxFreq = Math.max(maxFreq, freq);
+        }
+
+        // Bucket sort
+        List<Integer>[] buckets = new List[maxFreq + 1];
+        for(int num : frequencyMap.keySet()) {
+            int freq = frequencyMap.get(num);
+            if(buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(num);
+        }
+
+        // Collect top k elements
+        int[] topK = new int[k];
+        int index = 0;
+        for(int i = maxFreq; i >= 0 && index < k; i--) {
+            if(buckets[i] != null) {
+                for(int num : buckets[i]) {
+                    topK[index++] = num;
+                    if(index == k) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return topK;
+    }
+}
