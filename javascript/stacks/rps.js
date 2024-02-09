5708c8482eddbbf1450096f25de65bd77c712720
AxelLWitt <axellwitt16@gmail.com>
2024-02-08 19:18:19 -0500

    optimized rps

--- javascript/stacks/rps.js
+++ javascript/stacks/rps.js
@@ -28,3 +28,32 @@ var evalRPN = function (tokens) {
 };
 
 //unoptimized
+
+function symbolEval(num1, num2, symbol){
+    switch(symbol){
+        case '+':
+            return num2+num1
+        case '-':
+            return num2-num1
+        case '*':
+            return num2*num1
+        case '/':
+            let result = num2/num1
+            return (result < 0 ? Math.ceil(result) : Math.floor(result))
+    }
+}
+
+var evalRPN = function(tokens) {
+    let stack = [];
+    let symbols = ['+', '-', '/', '*']
+    tokens.forEach(x => {
+        if(symbols.includes(x)){
+            stack.push(symbolEval(stack.pop(), stack.pop(), x) )
+        }else{
+            stack.push(parseInt(x, 10))
+        }
+    })
+    return stack[0]
+};
+
+//optimized for time
