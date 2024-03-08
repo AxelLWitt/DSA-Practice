44f2a2b76305ea712d11dd4e7fcd8c031f4cc521
AxelLWitt <axellwitt16@gmail.com>
2024-03-08 17:05:47 -0500

    algo breakdown

--- java/arrays/containsDuplicate.java
+++ java/arrays/containsDuplicate.java
@@ -1,11 +1,16 @@
 import java.util.*;
 
 class Solution {
+    //function for finding duplicate
     public boolean containsDuplicate(int[] nums) {
+        //create a hashset to contain all the numbers
         Set<Integer> set = new HashSet<Integer> ();
+        //iterate through the nums array
         for(int i=0;i<nums.length;i++){
+            //add each of the nums to the hashset
             set.add(nums[i]);
         }
+        //return whether the hashset is equal to the input array as the set will contain no duplicates
         return set.size() != nums.length;
     }
 }
